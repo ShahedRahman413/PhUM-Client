b5687d1dca3baa14cc69c97519d5b18fd2d7cd33
ShahedRahman413 <githubshahed413@gmail.com>
2024-07-09 10:33:33 +0600

    uploading

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,94 @@
+import { Layout, Menu, MenuProps, theme } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: "Dashboard",
+  },
+  {
+    key: "2",
+    label: "Profile",
+  },
+  {
+    key: "3",
+    label: "User Management",
+    children: [
+      {
+        key: "3-1",
+        label: "Users",
+      },
+      {
+        key: "3-2",
+        label: "Roles",
+      },
+      {
+        key: "3-3",
+        label: "Permissions",
+      },
+    ],
+  },
+];
+
+const MainLayout = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <div>
+      <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div
+            style={{
+              color: "white",
+              textAlign: "center",
+              height: "4rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <h1>Ph Uni</h1>
+          </div>
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["4"]}
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "24px 16px 0" }}>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              content
+            </div>
+          </Content>
+          <h1>The main content should go here</h1>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    </div>
+  );
+};
+
+export default MainLayout;
